2349fa33b7f93657cc46ca65b6a1f22aec1f6373
aya haj ali <ayahajali02@gmail.com>
2024-03-24 15:03:03 +0100

    supprimer et creer new java andd xml

--- app/src/main/java/com/example/projetfedere/ui/login.java
+++ app/src/main/java/com/example/projetfedere/ui/login.java
@@ -1,14 +1,16 @@
-package com.example.projetfedere;
+package com.example.projetfedere.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.projetfedere.R;
+
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
