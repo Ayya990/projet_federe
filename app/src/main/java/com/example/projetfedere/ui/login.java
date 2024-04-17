e5ea62cf47804f264662650686768f7990af373b
maleklouka03 <malekferjani85@icloud.com>
2024-04-17 09:32:08 +0100

    hello

--- app/src/main/java/com/example/projetfedere/ui/login.java
+++ app/src/main/java/com/example/projetfedere/ui/login.java
@@ -6,22 +6,43 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.projetfedere.R;
 
 public class login extends AppCompatActivity {
+    private EditText email;
+    private EditText psswd;
+    private Button log;
+    private Button sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
         setContentView(R.layout.activity_login);
-    }
-    public void onLoginClick(View View){
-        startActivity(new Intent(this,register.class));
-        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
 
+        email = findViewById(R.id.email);
+        psswd = findViewById(R.id.password);
+        log = findViewById(R.id.login);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ema = email.getText().toString().trim();
+                String password = psswd.getText().toString().trim();
+                Intent i1 =new Intent(login.this, register.class);
+                startActivity(i1);
+            }
+        });
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ema = email.getText().toString().trim();
+                String password = psswd.getText().toString().trim();
+                Intent i1 =new Intent(login.this, dash.class);
+                startActivity(i1);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projetfedere/ui/register.java
+++ app/src/main/java/com/example/projetfedere/ui/register.java
@@ -9,27 +9,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.projetfedere.R;
 
 public class register extends AppCompatActivity {
+    private Button reg;
+    private EditText nom;
+    private EditText email;
+    private EditText pass;
+    private EditText phone;
+    private EditText niveau;
+    private EditText type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        reg = findViewById(R.id.btn);
+        nom = findViewById(R.id.name);
+        email = findViewById(R.id.email1);
+        pass = findViewById(R.id.psswd);
+        phone = findViewById(R.id.number);
+        niveau = findViewById(R.id.niv); // Assuming this is a Spinner for level selection
+        type = findViewById(R.id.type1);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ema = email.getText().toString().trim();
+                String password = pass.getText().toString().trim();
+                String nam = nom.getText().toString().trim();
+                String ph = phone.getText().toString().trim();
+                String ni = niveau.getText().toString().trim();
+                String ty = type.getText().toString().trim();
+                Intent i1 =new Intent(register.this,login.class);
+                startActivity(i1);
+            }
+        });
     }
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-            window.setStatusBarColor(getResources().getColor(R.color.register_bk_color));
-        }
-    }
-    public void onLoginClick(View view){
-        startActivity(new Intent(this,login.class));
-        overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right);
 
-    }
+
 }
